0d2ab10ac6e85de13cb4b55909ace484b894a93d
Leon <homik.lukasz@gmail.com>
2017-09-06 21:47:38 +0200

    MouseInput, GameManager

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public InputController inputController;
+
+	// Use this for initialization
+	void Start () {
+        inputController = GameManager.instance.inputController;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("Horizontal: " + inputController.MouseInput.x);
+        Debug.Log("Vertical: " + inputController.MouseInput.y);
+    }
+}
---
+++ Assets/Scripts/Shared/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameManager {
+
+    #region Singleton
+    private GameObject gameObject;
+
+    private static GameManager mInstance;
+    public static GameManager instance
+    {
+        get
+        {
+            if(mInstance == null)
+            {
+                mInstance = new GameManager();
+                mInstance.gameObject = new GameObject("_gameManager");
+                mInstance.gameObject.AddComponent<InputController>();
+            }
+            else
+            {
+                Debug.Log("GameManager already defined");
+            }
+            
+            return mInstance;
+        }
+
+    }
+    #endregion
+
+    private InputController mInputController;
+    public InputController inputController
+    {
+        get
+        {
+            if(mInputController == null)
+            {
+                mInputController = gameObject.GetComponent<InputController>();              
+            }
+            return mInputController;
+        }
+    }
+}
